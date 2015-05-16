a643dbfd4bf3c196c282bf8f8c5c1729198cfe6b
Tom Witkowski <dev.gummibeer@gmail.com>
2015-05-17 01:20:13 +0200

    add optional icons

--- partials/share.php
+++ partials/share.php
@@ -6,13 +6,13 @@
  * @link:	https://github.com/Gummibeer
  * @copyright:	Copyright (c) 2015, Tom Witkowski
  * @license:	http://www.gnu.org/licenses/gpl-3.0.html GNU GPL v3
- * @version:	v1.0
+ * @version:	v1.1
  */
 ?>
 
 <?php
 /**
- * List of Colors for CSS: http://brandcolors.net/
+ * List of Colors for CSS: http://brandcolors.net
  * List of Links: https://github.com/bradvin/social-share-urls
  *
  * buffer	http://bufferapp.com/add?text={title}&url={url}
@@ -41,8 +41,8 @@
  * {is_video}	If the content is a video or not
  */
 
+// enter your social-media services
 $services = [
-	// enter your social-media services
 	'facebook'	=> 'https://facebook.com/sharer.php?u={url}',
 	'twitter'	=> 'https://twitter.com/intent/tweet?text={title}&url={url}',
 	'google'	=> 'https://plus.google.com/share?url={url}',
@@ -56,6 +56,19 @@ $parameters = [
 	// or overwrite the default parameters
 ];
 
+// change the default settings
+$settings = [
+	'text'		=> 'auf {sm} teilen',	// string
+	'uc_sm'		=> true,		// boolean
+	'uc_words'	=> false,		// boolean
+	'iconset'	=> false,		// false || fa || whhg || custom icon set
+	'display'	=> 'text'		// text || icon || both
+];
+
+$iconsets = [
+	// enter your custom iconsets
+];
+
 /*
  |--------------------------------------------------------------------------
  | That's all, stop editing! Happy blogging.
@@ -64,19 +77,65 @@ $parameters = [
 ?>
 <ul id="share" class="list-unstyled row">
 	<?php
-	if(!isset($parameters['url'])) {
-		$parameters['url']	= urlencode( get_permalink() );
-	}
-	if(!isset($parameters['title'])) {
-		$parameters['title']	= urlencode( get_the_title() );
-	}
-	if(!isset($parameters['desc'])) {
-		$parameters['desc']	= urlencode( get_the_excerpt() );
+	// defaults
+	$iconsets['fa'] = [
+		'delicious'	=> 'fa fa-delicious',
+		'digg'		=> 'fa fa-digg',
+		'facebook'	=> 'fa fa-facebook',
+		'google'	=> 'fa fa-google-plus',
+		'linkedin'	=> 'fa fa-linkedin',
+		'pinterest'	=> 'fa fa-pinterest',
+		'reddit'	=> 'fa fa-reddit',
+		'stumbleupon'	=> 'fa fa-stumbleupon',
+		'tumblr'	=> 'fa fa-tumblr',
+		'twitter'	=> 'fa fa-twitter',
+		'xing'		=> 'fa fa-xing',
+	];
+	$iconsets['whhg'] = [
+		'delicious'	=> 'icon-delicious',
+		'digg'		=> 'icon-digg',
+		'facebook'	=> 'icon-facebook',
+		'google'	=> 'icon-googleplus',
+		'linkedin'	=> 'icon-linkedin',
+		'pinterest'	=> 'icon-pinterest',
+		'reddit'	=> 'icon-reddit',
+		'stumbleupon'	=> 'icon-stumbleupon',
+		'tumblr'	=> 'icon-tumblr',
+		'twitter'	=> 'icon-twitter',
+	];
+	$parameters['url'] = isset($parameters['url']) ? $parameters['url'] : urlencode( get_permalink() );
+	$parameters['title'] = isset($parameters['title']) ? $parameters['title'] : urlencode( get_the_title() );
+	$parameters['desc'] = isset($parameters['desc']) ? $parameters['desc'] : urlencode( get_the_excerpt() );
+
+	if($settings['iconset'] != false && isset($iconsets[$settings['iconset']]) && is_array($iconsets[$settings['iconset']])) {
+		$iconset = $iconsets[$settings['iconset']];
+	} else {
+		$iconset = false;
 	}
 	$i = 0;
 	$return = '';
-	foreach($services as $sm => $url) {
-		$link = '<a href="'.preg_replace("|{(\w*)}|e", '$parameters["$1"]', $url).'" class="btn btn-default btn-block btn-'.$sm.'" target="_blank">auf '.$sm.' teilen</a>';
+	foreach($services as $service => $url) {
+		$service = strtolower($service);
+		$plattform = $service;
+		if($settings['uc_sm']) {
+			$plattform = ucfirst($plattform);
+		}
+		$text = str_replace('{sm}', $plattform, $settings['text']);
+		if($settings['uc_words']) {
+			$text = ucwords($text);
+		}
+		$icon = '';
+		if($iconset != false && is_array($iconset) && isset($iconset[$service])) {
+			$icon = '<i class="' . $iconset[$service] . '"></i>';
+		}
+		$content = '';
+		if($settings['display'] == 'icon' || $settings['display'] == 'both') {
+			$content .= $icon . ' ';
+		}
+		if($settings['display'] == 'text' || $settings['display'] == 'both') {
+			$content .= '<span>'.$text.'</span>';
+		}
+		$link = '<a href="'.preg_replace("|{(\w*)}|e", '$parameters["$1"]', $url).'" class="btn btn-default btn-block btn-'.$service.'" target="_blank">'.$content.'</a>';
 		if($i < 3) {
 			if($i == 0 || count($services) == 3) {
 				$return .= '<li class="col-md-4">';
